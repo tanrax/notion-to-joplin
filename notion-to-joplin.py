06b52c79b12b3f9e30cbd0c69b70782f3a99205b
Andros Fenollosa <andros@fenollosa.email>
2023-10-29 18:09:13 +0100

    First commit

---
+++ notion-to-joplin.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Step 1: Unzip the notion export zip file
+# Step 2: Run this script in the same directory as the unzipped files
+# Step 3: Rename every file with a .md extension with the heading of the file and fix all the links
+# Step 4: Delete every heading file
+# Step 5: Delete blank lines in beginning of every file
+# Step 6: Rename all folder. Remove the "hash" from the ending of the folder name
+
+## Step 1
+# Get parameter
